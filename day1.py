5f73f195fca429a04dcdb06a17121cd0b5b5e7d4
Windyl Monton <windyl1.monton.lvhs@gmail.com>
2025-06-13 16:16:35 +0800

    Finished 1 day of python

---
+++ day1.py
@@ -0,0 +1,48 @@
+#Comment
+"""Multi line comment"""
+
+# * Print statement in python
+#print("hello");
+
+
+#=====================================================================
+#*Python Datatypes:
+
+#Number - -3, 1, 0
+#Float - -3.5 , 2.5
+#Complex  - 1 + j , 2 +4j
+#String - 'Asabeneh' , 'Python"
+#Boolean  - True , False
+#List  - [0,2,3,4,5], ["Windyl","Carl","John"] , ["Windyl",0.2,"John"]
+#Dictionary 
+"""
+{
+'first_name':'Asabeneh',
+'last_name':'Yetayeh',
+'country':'Finland', 
+'age':250, 
+'is_married':True,
+'skills':['JS', 'React', 'Node', 'Python']
+}
+"""
+# ! Cannot be modified unlike lists
+#Tuple - ('Asabeneh', 'Pawel', 'Brook', 'Abraham', 'Lidiya')
+#! Order is not important in set
+#Set - {2,3,4,5}
+#=====================================================================
+#* Checking Data Types
+# Type function is used to check the type of a data in python
+
+print(type(10)) # <class 'int'>
+print(type(0.5)) # <class 'float'>
+print(type(1j)) # <class 'complex'>
+print(type("Windyl")) # <class 'str'>
+print(type(True)) # <class 'bool'>
+print(type([0,1,2,3,"Windyl"])) # <class 'list'>
+print(type({'first_name':"Windyl", "last_name" : "Monton"})) #<class 'dict'>
+print(type(("Mama", "Papa"))) # <class 'tuple'>
+print(type({2,3,4,5})) # <class 'set'>
+#=====================================================================
+#* Basic operations
+
+
---
+++ exercise1.py
@@ -0,0 +1,11 @@
+#Find an Euclidian distance between (2, 3) and (10, 8)
+
+print("Enter number1:")
+num1 = int(input())
+print("Enter number2:")
+num2 = int(input())
+
+distance = num1 - num2
+
+print("Euclidean Distance:")
+print(distance)
